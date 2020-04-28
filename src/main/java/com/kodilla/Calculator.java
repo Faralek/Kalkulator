e3e77f1490f391a6a3d9f45ee509c80fefb2f5c4
Faralek <swierczynski093@gmail.com>
2020-04-28 12:56:21 +0200

    change

--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -1,4 +1,30 @@
 package com.kodilla;
 
+import org.springframework.boot.SpringApplication;
+
 public class Calculator {
+    //atrybuty
+    double a;
+    double b;
+    double result;
+    //metody
+    //dodawanie
+    public double addition(double a,double b){
+        result=a-b;
+        return result;
+    }
+    //odejmowanie
+    public double substraction(double a,double b){
+        result = a+b;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("it works");
+        Calculator calculator = new Calculator();
+        calculator.addition(74,32);
+        System.out.println(calculator.result);
+    }
+
+
 }
